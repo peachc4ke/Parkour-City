dd98b1522bdc5c8d38630508251d51210c5740f6
sumin-tail <56198155+sumin-tail@users.noreply.github.com>
2020-11-03 21:12:23 +0900

    Add files via upload
    맵회전 코드

---
+++ SpawnTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrigger : MonoBehaviour
+{
+    //현재 맵이 뭔지 넣기위해
+    public map m;
+
+    //기본형은 본인에게 다른것(뭐든지)이 닿았을 때
+    public void OnTriggerEnter(Collider other)
+    {
+        //이렇게 실행하면 한번에 90도 가 확 돌아가게 됨-순간이동 처럼
+        //m.transform.Rotate(new Vector3(0, 90, 0));
+        //코루틴-프레임과 상관없이 별도의 서브 루틴에서 원하는 작업을 원하는 시간만큼 수행
+        StartCoroutine(rotateMap());
+    }
+
+    public IEnumerator rotateMap()
+    {
+        //15번 반복/한번 실행될때마다 맵을 6도씩 돌림
+        for (int i = 0; i < 15; i++)
+        {
+            //현재 맵이 들어간 m을 돌림.
+            m.transform.Rotate(new Vector3(0, 6, 0));
+            //yield return new WaitForSeconds() < 함수는  0.02초 동안 잠시 멈추라는 의미를 가진 코드
+            yield return new WaitForSeconds(0.02f);
+        }
+        //== 화면이 0.3 초에 걸쳐서 돌아가게 됨
+    }
+}
---
+++ map.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class map : MonoBehaviour
+{
+    
+    void Update()
+    {
+        //트랜스폼/x,y 건들지 않고 z축만 - 방향으로 
+        //deltaTime =한 프레임 당 실행하는 시간/단위는 초 
+        //기준을 월드 좌표계로 잡음
+        //로컬좌표계에서 z축은 무조건 바라보고있는 방향
+        //월드좌표계에서는 바라보는 방향과 상관없이 고정된 한축의 방향
+        //로컬좌표계로 Translate쓰면 이동방향도 같이 90도로 이동함
+        //방지하기 위해 월드 좌표 계 생성/회전과 상관없이 무조건 플레이어 반대 방향으로 맵이 이동해야하니
+        transform.Translate(new Vector3(0,0,-2f*Time.deltaTime), Space.World);
+    }
+
+}
