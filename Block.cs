b6c0aca2876384b6cf06e34f21879e4f072b9b92
peachc4ke <56034887+peachc4ke@users.noreply.github.com>
2020-11-13 16:16:29 +0900

    장애물 코드

---
+++ Block.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("hit");
+            HPManager.Life -= 1;
+            Destroy(gameObject);
+        }
+    }
+}
