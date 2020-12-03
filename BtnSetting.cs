e31f94294808e4efe79f341a07304ca00db1ca48
jiHyeon Choi <56023629+Sang-O@users.noreply.github.com>
2020-12-04 02:35:35 +0900

    Add files via upload

---
+++ BtnSetting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnSetting : MonoBehaviour
+{
+    private bool state;
+    void Start()
+    {
+        state = false;
+    }
+    public GameObject Sound;
+    public void OnSetting()
+    {
+        if(state == false)
+        {
+            Sound.SetActive(true);
+            state = true;
+        }
+        else
+        {
+            Sound.SetActive(false);
+            state = false;
+        }
+    }
+}
