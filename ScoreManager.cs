ca2999941ce2b34844e801e47e45153a6e4899cd
sumin-tail <56198155+sumin-tail@users.noreply.github.com>
2020-12-04 02:48:14 +0900

    Add files via upload
    돌아가지 않는 오류 수정

---
+++ ScoreManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.Log(" ");
+            Destroy(this);
+            return;
+        }
+
+        instance = this;
+    }
+
+    public Text scoreText; //점수를 표시하는 Text 객체
+    public int _Score = 0; //점수 관리
+
+    void Update()
+    {
+        scoreText.text = " " + _Score; //텍스트 반영
+    }
+}
