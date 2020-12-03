a5a0c2b5630eac7bfbfca44ef5e84c9cec621ee8
jiHyeon Choi <56023629+Sang-O@users.noreply.github.com>
2020-12-04 02:56:07 +0900

    Add files via upload

---
+++ Game_Exit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game_Exit : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+
+        #else
+            Application.Quit();
+        
+        #endif
+    }
+
+}
