253a8056f782d9f2f5e679a60d5ab22ac0cfcd50
sumin-tail <56198155+sumin-tail@users.noreply.github.com>
2020-12-10 21:32:37 +0900

    Add files via upload
    무브 코드 수정과
    카메라 움직임 기능 ( 3D멀미로 인해 인게임에서는 제외

---
+++ Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    void Update()
+    {
+
+        ////위치 변경 - 왼쪽
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    transform.Translate(Vector3.left * 0.5f * Time.deltaTime);
+        //}
+
+        ////위치변경 - 오른쪽
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    transform.Translate(Vector3.right * 0.5f * Time.deltaTime);
+        //}
+    }
+}
