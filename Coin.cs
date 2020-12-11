6a416c6c693a775da61516005b26d22222f56c7c
sumin-tail <56198155+sumin-tail@users.noreply.github.com>
2020-12-11 12:46:03 +0900

    Add files via upload
    점수에 따른 배경음악 변경 기능과 코인 색상에 따라 다른 코인습득 소리

---
+++ Coin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (gameObject.tag == "Gold")//금일 때
+            {
+                SoundManager.PlaySound("GoldCoin");
+                ScoreManager._Score += 500;
+            }
+            else if (gameObject.tag == "Silver")//은일 때
+            {
+                SoundManager.PlaySound("Coin");
+                ScoreManager._Score += 300;
+            }
+            else if (gameObject.tag == "Bronze")
+            {
+                SoundManager.PlaySound("Coin");
+                ScoreManager._Score += 100;
+            }
+
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ SoundManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public static AudioClip CoinSound, GoldCoinSound;
+    static AudioSource audioSrc;
+
+    public AudioClip background_music;
+    public AudioClip Score_music;
+    public AudioClip Scoreup_music;
+
+    //스코어 체크 
+    public int Score = 0;
+
+    void Start()
+    {
+        CoinSound = Resources.Load<AudioClip>("Coin");
+        GoldCoinSound = Resources.Load<AudioClip>("GoldCoin");
+
+        background_music = Resources.Load<AudioClip>("Stage1");
+        Score_music = Resources.Load<AudioClip>("Stage2");
+        Scoreup_music = Resources.Load<AudioClip>("Stage3");
+
+        audioSrc = GetComponent<AudioSource>();
+
+        audioSrc.clip = background_music;
+        audioSrc.Play();
+        audioSrc.loop = true;
+
+    }
+
+    private void Update()
+    {
+        if(ScoreManager._Score >= 5000 && ScoreManager._Score < 10000)
+        {
+            if (Score < 5000)
+            {
+                if (audioSrc.isPlaying)
+                {
+                    audioSrc.loop = false;
+                    audioSrc.Stop();
+                }
+                audioSrc.clip = Score_music;
+                audioSrc.Play();
+                audioSrc.loop = true;
+
+                Score = 5000;
+            }
+           
+        }
+        else if (ScoreManager._Score >= 10000)
+        {
+            if (Score < 10000)
+            {
+                if (audioSrc.isPlaying)
+                {
+                    audioSrc.loop = false;
+                    audioSrc.Stop();
+                }
+                audioSrc.clip = Scoreup_music;
+                audioSrc.Play();
+                audioSrc.loop = true;
+                Score = 10000;
+            }
+           
+        }
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            case "Coin":
+                audioSrc.PlayOneShot(CoinSound);
+                break;
+            case "GoldCoin":
+                audioSrc.PlayOneShot(GoldCoinSound);
+                break;
+        }
+    }
+
+
+}
